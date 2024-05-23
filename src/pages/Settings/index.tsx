e3979f66799276e4be6306b855aceba4200261cf
Ashraf Yusuf <ashrafowuda@yahoo.com>
2024-05-23 10:27:40 +0100

    Checkpoint: pre-deploy

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/containers/Layout'
 
 const Settings = (): JSX.Element => {
-  return <Layout hasSearchBar={false} />
+  return <Layout hasSearchBar={false} searchBarValue='' setSearchBarValue={() => {}} />
 }
 
 export default Settings
--- src/routes/unprotected.tsx
+++ src/routes/unprotected.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Login from '@/pages/Login'
-import Register from '@/pages/Register'
 import { routes } from '@/constants'
+// import Login from '@/pages/Login'
+// import Register from '@/pages/Register'
+import Dashboard from '@/pages/Dashboard'
 
 const UnprotectedRoutes = () => {
   return (
     <Routes>
-      <Route path={routes.home} element={<Login />} />
-      <Route path={routes.login} element={<Login />} />
-      <Route path={routes.register} element={<Register />} />
+      <Route path={routes.home} element={<Dashboard />} />
+      {/* <Route path={routes.login} element={<Login />} />
+      <Route path={routes.register} element={<Register />} /> */}
     </Routes>
   )
 }
